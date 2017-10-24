c23ba6baaf3bb7fc7511c8f1524a0f99f3274de3
Timothy <tamadi293@gmail.com>
2017-10-24 16:53:34 -0400

    Add files via upload

--- Square.js
+++ Square.js
@@ -4,7 +4,7 @@ class Square extends Component {
   render() {
     const initStyling = {width: '50px', height: '50px', backgroundColor: randomColor()}
     function randomColor() {
-      var colors = ['red', 'yellow', 'orange', 'green', 'blue']
+      var colors = ['red', 'yellow', 'orange', 'green', 'blue', 'lightblue']
       return colors[Math.floor(Math.random() * colors.length)]
     }
     return (
--- index.js
+++ index.js
@@ -1,11 +1,52 @@
-module.exports = function (xs, f) {
-    if (xs.map) return xs.map(f);
-    var res = [];
-    for (var i = 0; i < xs.length; i++) {
-        var x = xs[i];
-        if (hasOwn.call(xs, i)) res.push(f(x, i, xs));
-    }
-    return res;
-};
+/*!
+ * align-text <https://github.com/jonschlinkert/align-text>
+ *
+ * Copyright (c) 2015, Jon Schlinkert.
+ * Licensed under the MIT License.
+ */
 
-var hasOwn = Object.prototype.hasOwnProperty;
+'use strict';
+
+var typeOf = require('kind-of');
+var repeat = require('repeat-string');
+var longest = require('longest');
+
+module.exports = function alignText(val, fn) {
+  var lines, type = typeOf(val);
+
+  if (type === 'array') {
+    lines = val;
+  } else if (type === 'string') {
+    lines = val.split(/(?:\r\n|\n)/);
+  } else {
+    throw new TypeError('align-text expects a string or array.');
+  }
+
+  var fnType = typeOf(fn);
+  var len = lines.length;
+  var max = longest(lines);
+  var res = [], i = 0;
+
+  while (len--) {
+    var line = String(lines[i++]);
+    var diff;
+
+    if (fnType === 'function') {
+      diff = fn(line.length, max.length, line, lines, i);
+    } else if (fnType === 'number') {
+      diff = fn;
+    } else {
+      diff = max.length - line.length;
+    }
+
+    if (typeOf(diff) === 'number') {
+      res.push(repeat(' ', diff) + line);
+    } else if (typeOf(diff) === 'object') {
+      var result = repeat(diff.character || ' ', diff.indent || 0);
+      res.push((diff.prefix || '') + result + line);
+    }
+  }
+
+  if (type === 'array') return res;
+  return res.join('\n');
+};
