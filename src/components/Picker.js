916792c562108421dc84b89fad9aa3bc020c124a
Timothy Amadi <timothy@Timothys-Air.attlocal.net>
2017-10-24 16:58:41 -0400

    Add the files(non VIA upload)

---
+++ src/components/Picker.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Picker extends Component  {
+  constructor(props) {
+    super(props)
+
+    const bgColor = this.props.bgColor
+  }
+  render() {
+    const pickerStyle = {borderRadius: '50%', marginLeft: '5px', marginRight: '5px', borderRadius: '50px', minWidth: '50px', minHeight: '50px', height: '50px', width: '50px', backgroundColor: 'red'}
+    return (
+      <div className='picker' style={pickerStyle} onClick={this.props.bgColor}></div>
+    )
+  }
+}
+
+export default Picker
---
+++ src/components/Square.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Square extends Component {
+  render() {
+    const initStyling = {width: '50px', height: '50px', backgroundColor: randomColor()}
+    function randomColor() {
+      var colors = ['red', 'yellow', 'orange', 'green', 'blue', 'lightblue']
+      return colors[Math.floor(Math.random() * colors.length)]
+    }
+    return (
+      <div className="Square cell-12">
+        <div style={initStyling}>
+        </div>
+      </div>
+    );
+  }
+}
+export default Square
